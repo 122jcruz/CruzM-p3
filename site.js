ea8df1f5bd5ae18479e0c319ba9c7de28e5dedea
122jcruz <122jcruzmorales@frhsd.com>
2022-03-18 14:03:15 -0400

    Second commit

--- site.js
+++ site.js
@@ -2,10 +2,11 @@
 //
 */
 
-/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
+/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA*/
 //
 //    They are in the following format:
 //    title (String): the name of the movie
+var title = "IMDB + Josh’s Top 8 Movies"
 //    iscore (Number): the IMDB score
 //    rating (String): the movie's MPAA rating
 //    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
@@ -18,7 +19,7 @@
 //    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
 //
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-*/
+
 
 
 const vue_app = Vue.createApp({
